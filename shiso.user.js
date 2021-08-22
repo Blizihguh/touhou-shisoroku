5c80b73db8a925f8607d1b8e89554e457ef92ea3
Blizihguh <blizihguh@gmail.com>
2021-08-22 07:41:54 -0400

    Made fullsize cards bigger

--- shiso.user.js
+++ shiso.user.js
@@ -9,39 +9,65 @@
 // @grant        GM_addStyle
 // ==/UserScript==
 
-function fix_caps(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
+GM_addStyle(`
+    .game-page .full-card-art
+    {
+    	background-size: contain;
+    }
+    .game-page .full-card-template
+    {
+    	width: 120% !important;
+    	background-size: contain !important;
+    }
+    .full-card-text-container
+    {
+    	visibility: hidden;
+    }
+`);
 
 function get_new_mini_art(str) {
 	let regex = /art\/(.+)\/(.+).jpg/;
 	let matches = str.match(regex);
-	return "url('https://raw.githubusercontent.com/Blizihguh/touhou-shisoroku/master/img/" + matches[1] + "/crop/" + matches[2] + ".png')";
+	if (matches != null) { return "url('https://raw.githubusercontent.com/Blizihguh/touhou-shisoroku/master/img/" + matches[1] + "/crop/" + matches[2] + ".png')"; }
+}
+
+function get_new_full_art(str) {
+	let regex = /art\/(.+)\/(.+).jpg/;
+	let matches = str.match(regex);
+	if (matches != null) { return "url('https://raw.githubusercontent.com/Blizihguh/touhou-shisoroku/master/img/" + matches[1] + "/full/" + matches[2] + ".png')"; }
+}
+
+function replace_all_cards() {
+	// Replace mini card art
+	var card_divs = document.getElementsByClassName("mini-card-art");
+	for (var i=0; i<card_divs.length; i++) {
+		card_divs[i].style.backgroundImage = get_new_mini_art(card_divs[i].style.backgroundImage);
+	}
+
+	// Replace full card art
+	card_divs = document.getElementsByClassName("full-card-art");
+	for (var i=0; i<card_divs.length; i++) {
+		card_divs[i].previousElementSibling.style.backgroundImage = get_new_full_art(card_divs[i].style.backgroundImage);
+	}
+	//TODO: Make the corresponding "full-card-border" div invisible, and give the card image div the corresponding glow from full-card-border's color style
+
+	// Replace card backs
+	// card_divs = document.getElementsByClassName("full-card-template");
+	// for (var i=0; i<card_divs.length; i++) {
+	// 	card_divs[i].style.backgroundImage = "url('https://raw.githubusercontent.com/Blizihguh/touhou-shisoroku/master/img/Card%20Back.png')";
+	// }
 }
 
 function foo() {}
 
 angular.element(document.body).injector().invoke(['$rootScope', function(rootScope) {
-    rootScope.$on(Events.GAME_PAGE_LOADED, function (info) {
-        // Replace mini card art
-		var card_divs = document.getElementsByClassName("mini-card-art");
-		for (var i=0; i<card_divs.length; i++) {
-			card_divs[i].style.backgroundImage = get_new_mini_art(card_divs[i].style.backgroundImage);
-		}
+	rootScope.$on(Events.GAME_PAGE_LOADED, function (info) {
+		console.log(Events);
+		replace_all_cards();
+	});
 
-		// Replace full card art
-		card_divs = document.getElementsByClassName("full-card-art");
-		for (var i=0; i<card_divs.length; i++) {
-			//card_divs[i].style.backgroundImage = "url('https://raw.githubusercontent.com/Blizihguh/touhou-shisoroku/master/img/Festival.png')";
-		}
-
-		// Replace landscape art
-		// .game-page .landscape-art,
-
-    });
-
-    rootScope.$on(Events.CARD_STUDY_REQUEST, foo);
-    rootScope.$on(Events.LANDMARK_STUDY_REQUEST, foo);
-    rootScope.$on(Events.EVENT_STUDY_REQUEST, foo);
-    rootScope.$on(Events.GLOBAL_CLICK, foo);
+	rootScope.$on(Events.CARD_STUDY_REQUEST, foo);
+	rootScope.$on(Events.LANDMARK_STUDY_REQUEST, foo);
+	rootScope.$on(Events.EVENT_STUDY_REQUEST, foo);
+	rootScope.$on(Events.HAND_UPDATE, replace_all_cards);
 }]);
