7bd613c41ef573eed2833a07e8086a468ce1b8c0
Blizihguh <blizihguh@gmail.com>
2021-08-23 10:59:17 -0400

    First pass at card study replacement

--- shiso.user.js
+++ shiso.user.js
@@ -10,28 +10,28 @@
 // ==/UserScript==
 
 GM_addStyle(`
-    .game-page .full-card-art
-    {
-    	background-size: contain;
-    }
-    .game-page .full-card-template
-    {
-    	width: 120% !important;
-    	background-size: contain !important;
-    	visibility: visible !important;
-    	border: 16px solid rbga(0, 0, 0, 1);
-    	border-style: solid;
-    	border-radius: 16px;
-    }
-    .full-card-text-container
-    {
-    	visibility: hidden;
-    }
-    .full-card
-    {
-    	border-style: none !important;
-    	border-radius: 0px;
-    }
+	.game-page .full-card-art
+	{
+		background-size: contain;
+	}
+	.game-page .full-card-template
+	{
+		width: 120% !important;
+		background-size: contain !important;
+		visibility: visible !important;
+		border: 16px solid rbga(0, 0, 0, 1);
+		border-style: solid;
+		border-radius: 16px;
+	}
+	.full-card-text-container
+	{
+		visibility: hidden;
+	}
+	.full-card
+	{
+		border-style: none !important;
+		border-radius: 0px;
+	}
 `);
 
 function getSiblingBySelector(elem, selector) {
@@ -60,6 +60,26 @@ function get_new_full_art(str) {
 	if (matches != null) { return "url('https://raw.githubusercontent.com/Blizihguh/touhou-shisoroku/master/img/" + matches[1] + "/full/" + matches[2] + ".png')"; }
 }
 
+function replace_card_study() {
+	var observer = new MutationObserver(function (mutations, me) {
+	  // Wait for the study window to load
+	  var study_card_divs = document.getElementsByClassName("card-study-window");
+	  if (study_card_divs[0]) {
+	  	// Get the template, to paste the full card art over
+		var study_card = study_card_divs[0].querySelector(".full-card-template");
+		// Get the name of the card
+		var card_url = getSiblingBySelector(study_card, ".full-card-art").style.backgroundImage;
+		// Replace image
+		study_card.style.backgroundImage = get_new_full_art(card_url);
+		// Exit from observer
+		me.disconnect();
+		return;
+	  }
+	});
+
+	observer.observe(document, {childList: true, subtree: true});
+}
+
 function replace_all_cards() {
 	// Replace mini card art
 	var card_divs = document.getElementsByClassName("mini-card-art");
@@ -79,8 +99,10 @@ function replace_all_cards() {
 		if (treasureBar) { treasureBar.style.visibility = "hidden"; }
 
 	}
+	//TODO: Fix cards only having rounded corners on the left and not on the right...?
 	//TODO: Give the card image div the corresponding glow from full-card-border's color style
-	//TODO: Replace card image on right click
+	//TODO: Council Room doesn't work on right click?
+	//TODO: Remove card text on right click
 	//TODO: Replace card backs
 	//TODO: Fix cards sometimes not updating after animations (eg when buying with multiple buys, when playing treasures that don't stack)
 	//TODO: Fix opponents' cards just being totally fucked up
@@ -94,7 +116,7 @@ angular.element(document.body).injector().invoke(['$rootScope', function(rootSco
 		replace_all_cards();
 	});
 
-	rootScope.$on(Events.CARD_STUDY_REQUEST, foo);
+	rootScope.$on(Events.CARD_STUDY_REQUEST, replace_card_study);
 	rootScope.$on(Events.LANDMARK_STUDY_REQUEST, foo);
 	rootScope.$on(Events.EVENT_STUDY_REQUEST, foo);
 	rootScope.$on(Events.HAND_UPDATE, replace_all_cards);
