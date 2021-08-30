aa99f247fc192a871c65f2b640575cc4c226ca8d
Blizihguh <blizihguh@gmail.com>
2021-08-29 21:37:21 -0400

    Made card backs, stack numbers, and x's visible

--- ff-ext/background.js
+++ ff-ext/background.js
@@ -1,9 +1,14 @@
-var pattern = "https://dominion.games/images/cards/art/base/*";
+var back = "https://dominion.games/images/cards/templates/back.jpg"
+var base = "https://dominion.games/images/cards/art/base/*";
 
 function get_new_full_art(url) {
 	let regex = /art\/(.+)\/(.+).jpg/;
 	let matches = url.match(regex);
 	if (matches != null) { return "https://raw.githubusercontent.com/Blizihguh/touhou-shisoroku/master/img/" + matches[1] + "/full/" + matches[2] + ".png"; }
+	// Handle card backs
+	if (url == "https://dominion.games/images/cards/templates/back.jpg") {
+		return "https://raw.githubusercontent.com/Blizihguh/touhou-shisoroku/master/img/back.png";
+	}
 }
 
 function redirect(requestDetails) {
@@ -15,6 +20,6 @@ function redirect(requestDetails) {
 
 browser.webRequest.onBeforeRequest.addListener(
 	redirect,
-	{urls:[pattern], types:["image"]},
+	{urls:[back, base], types:["image"]},
 	["blocking"]
 );
--- ff-ext/touhou.js
+++ ff-ext/touhou.js
@@ -10,6 +10,11 @@ function replace_mini_art() {
 	var card_divs = document.getElementsByClassName("mini-card-art");
 	for (var i=0; i<card_divs.length; i++) {
 		card_divs[i].style.backgroundImage = get_new_mini_art(card_divs[i].style.backgroundImage);
-		clearInterval(load_cards); // Only load cards once
+		//Can't do this if we want this to work for multiple games, unless we have some way of catching new game events
+		//clearInterval(load_cards); // Only load cards once
 	}
 }
+
+
+/*TODO: Something is making playable cards highlight green on hover??? */
+/*TODO: Infinite load on undo */
