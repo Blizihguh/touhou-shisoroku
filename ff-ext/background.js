8f2cff97ae83569c24e879b508fabff26086c21d
Blizihguh <blizihguh@gmail.com>
2021-10-26 01:06:01 -0500

    Fixed new cards' study art and mini-prosperity base card art

--- ff-ext/background.js
+++ ff-ext/background.js
@@ -1,13 +1,29 @@
-var back = "https://dominion.games/images/cards/templates/back.jpg"
+var back = "https://dominion.games/images/cards/templates/back.jpg";
+var alch = "https://dominion.games/images/cards/art/alchemy/*";
 var base = "https://dominion.games/images/cards/art/base/*";
+var corn = "https://dominion.games/images/cards/art/cornucopia/*";
+var dark = "https://dominion.games/images/cards/art/dark-ages/*";
+var gild = "https://dominion.games/images/cards/art/guilds/*";
+var hint = "https://dominion.games/images/cards/art/hinterlands/*";
+var intr = "https://dominion.games/images/cards/art/intrigue/*";
+var prmo = "https://dominion.games/images/cards/art/promos/*";
+var prsp = "https://dominion.games/images/cards/art/prosperity/*";
+var seas = "https://dominion.games/images/cards/art/seaside/*";
 
 function get_new_full_art(url) {
 	let regex = /art\/(.+)\/(.+).jpg/;
 	let matches = url.match(regex);
+
 	if (matches != null) { return "https://raw.githubusercontent.com/Blizihguh/touhou-shisoroku/master/img/" + matches[1] + "/full/" + matches[2] + ".png"; }
-	// Handle card backs
+	// Handle card backs, platinum, and colony
 	if (url == "https://dominion.games/images/cards/templates/back.jpg") {
 		return "https://raw.githubusercontent.com/Blizihguh/touhou-shisoroku/master/img/back.png";
+	}
+	else if (url == "https://dominion.games/images/cards/art/prosperity/mini-platinum.jpg") { // For some reason plat and colony are stored as png instead of jpg ._.
+		return "https://raw.githubusercontent.com/Blizihguh/touhou-shisoroku/master/img/prosperity/mini-platinum.png";
+	}
+	else if (url == "https://dominion.games/images/cards/art/prosperity/mini-colony.jpg") {
+		return "https://raw.githubusercontent.com/Blizihguh/touhou-shisoroku/master/img/prosperity/mini-colony.png";
 	}
 }
 
@@ -20,6 +36,6 @@ function redirect(requestDetails) {
 
 browser.webRequest.onBeforeRequest.addListener(
 	redirect,
-	{urls:[back, base], types:["image"]},
+	{urls:[back, alch, base, corn, dark, gild, hint, intr, prmo, prsp, seas], types:["image"]},
 	["blocking"]
 );
